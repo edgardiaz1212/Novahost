9027499ebc750d4c88841e60e57c43f4b2dc8955
Edgar Diaz <ed35376@gmail.com>
2025-03-20 10:02:15 -0400

    navbar

--- src/front/js/component/navbar.jsx
+++ src/front/js/component/navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../../styles/Navbar.css';
 
 
 function Navbar({ isOpen, toggleMenu }) {
   return (
-    <div className={`offcanvas offcanvas-start ${isOpen ? 'show' : ''}`} tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+    <div className={`offcanvas offcanvas-start w-25 ${isOpen ? 'show' : ''}`} tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
       <div className="offcanvas-header">
-        <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
+        <h5 className="offcanvas-title" id="offcanvasNavbarLabel">NovaHost</h5>
         <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" onClick={toggleMenu}></button>
       </div>
       <div className="offcanvas-body">
