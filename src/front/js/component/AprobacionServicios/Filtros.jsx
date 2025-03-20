372efea4dd0eb4dc1e7238cfb802c989d2f78449
Edgar Diaz <ed35376@gmail.com>
2025-03-20 14:06:54 -0400

    forma

--- src/front/js/component/AprobacionServicios/Filtros.jsx
+++ src/front/js/component/AprobacionServicios/Filtros.jsx
@@ -1,27 +1,36 @@
-import React from 'react';
-import { Search } from 'lucide-react';
+import React from "react";
+import { Search } from "lucide-react";
 
-function Filtros({ filtro, setFiltro, ordenPor, setOrdenPor, ordenAsc, setOrdenAsc, cambiarOrden }) {
+function Filtros({
+  filtro,
+  setFiltro,
+  ordenPor,
+  setOrdenPor,
+  ordenAsc,
+  setOrdenAsc,
+  cambiarOrden,
+}) {
   return (
     <div className="mb-6">
       <div className="flex justify-between items-center">
-        <div className="relative">
+        <div className="input-group relative" >
           <input
             type="text"
             placeholder="Buscar por solicitante o sistema operativo..."
-            className="pl-8 pr-4 py-2 border rounded-lg w-80"
+            className=" py-2 border rounded-2 w-25"
             value={filtro}
             onChange={(e) => setFiltro(e.target.value)}
+            aria-describedby="search-button"
           />
-          <div className="absolute left-3 top-3 text-gray-400">
+          <span className="input-group-text" id="search-button">
             <Search />
-          </div>
+          </span>
         </div>
 
-        <div className="flex items-center space-x-2">
-          <span className="text-sm text-gray-500">Ordenar por:</span>
+        <div className="flex items-center m-2">
+          <span className="text-sm ">Ordenar por:</span>
           <select
-            className="border rounded-md p-2"
+            className="border rounded-2 p-2"
             value={ordenPor}
             onChange={(e) => {
               setOrdenPor(e.target.value);
@@ -34,7 +43,7 @@ function Filtros({ filtro, setFiltro, ordenPor, setOrdenPor, ordenAsc, setOrdenA
             <option value="procesador">Procesador</option>
           </select>
           <button
-            className="p-2 border rounded-md"
+            className="p-2 border rounded-2 bg-secondary-subtle"
             onClick={() => setOrdenAsc(!ordenAsc)}
           >
             {ordenAsc ? "↑ Ascendente" : "↓ Descendente"}
