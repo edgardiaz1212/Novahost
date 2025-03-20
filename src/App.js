8b9e509f2e310808f28739eb71094c742f5ce484
Edgar Diaz <ed35376@gmail.com>
2025-03-20 09:30:28 -0400

    dashboard basico

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import HomePage from './front/js/pages/Home.jsx';
 import ServiceDetails from './front/js/pages/ServiceDetails.js'; // Import ServiceDetails
 import ServiceForm from './front/js/pages/ServiceForm.js'; // Import ServiceForm
 import injectContext from './front/js/store/appContext'; // Import injectContext
+import Dahsboard from './front/js/pages/Dahsboard.jsx';
 
 const App = () => {
     return (
         <Router>
             <Routes> {/* Use Routes instead of Switch */}
                 <Route path="/" element={<HomePage />} /> {/* Use element prop */}
-                <Route path="/services/:id" element={<ServiceDetails />} /> {/* Route for service details */}
+                <Route path="/dashboard" element={<Dahsboard />} /> {/* Route for dashboard */}
+                <Route path="/services/:id" element={<ServiceDetails />} /> 
                 <Route path="/services/create" element={<ServiceForm />} /> {/* Route for creating a service */}
                 <Route path="/services/edit/:id" element={<ServiceForm />} /> {/* Route for editing a service */}
             </Routes>
