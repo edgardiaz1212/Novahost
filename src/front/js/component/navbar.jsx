953b2a95ffdf29050c33f0a18b9d476eca10f890
Edgar Diaz <ed35376@gmail.com>
2025-03-27 12:49:39 -0400

    cambio forma

--- src/front/js/component/navbar.jsx
+++ src/front/js/component/navbar.jsx
@@ -3,6 +3,8 @@ import { Link, useNavigate } from 'react-router-dom';
 import '../../styles/Navbar.css';
 import { Context } from '../store/appContext';
 import { toast } from 'react-toastify';
+// Import Lucide Icons
+import { LayoutDashboard, Server, CheckCircle, Settings, LogOut } from 'lucide-react';
 
 function Navbar({ isOpen, toggleMenu }) {
   const { store, actions } = useContext(Context);
@@ -41,18 +43,31 @@ function Navbar({ isOpen, toggleMenu }) {
     <div className={`offcanvas offcanvas-start w-25 ${isOpen ? 'show' : ''}`} tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
       <div className="offcanvas-header">
         <h5 className="offcanvas-title" id="offcanvasNavbarLabel">NovaHost</h5>
+        <hr className="title-greeting-divider" /> {/* Separator Line */}
+        {/* Greeting and User Name - Now placed correctly */}
+        {store.isAuthenticated && store.user && (
+          <div className="user-greeting">
+            <p>Hola, {store.user.userName}!</p>
+          </div>
+        )}
         <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" onClick={toggleMenu}></button>
       </div>
       <div className="offcanvas-body d-flex flex-column">
         <ul className="navbar-nav justify-content-end pe-3">
           <li className="nav-item">
-            <Link to="/dashboard" className="nav-link" onClick={toggleMenu}>Dashboard</Link>
+            <Link to="/dashboard" className="nav-link" onClick={toggleMenu}>
+              <LayoutDashboard className="nav-icon" /> Dashboard
+            </Link>
           </li>
           <li className="nav-item">
-            <Link to="/service-selector" className="nav-link" onClick={toggleMenu}>Seleccion Servicio</Link>
+            <Link to="/service-selector" className="nav-link" onClick={toggleMenu}>
+              <Server className="nav-icon" /> Seleccion Servicio
+            </Link>
           </li>
           <li className="nav-item">
-            <Link to="/aprobacion" className="nav-link" onClick={toggleMenu}>Aprobacion Servicio</Link>
+            <Link to="/aprobacion" className="nav-link" onClick={toggleMenu}>
+              <CheckCircle className="nav-icon" /> Aprobacion Servicio
+            </Link>
           </li>
           {/* Add more navigation links here */}
         </ul>
@@ -60,12 +75,14 @@ function Navbar({ isOpen, toggleMenu }) {
           <hr className="logout-divider" />
           <ul className="navbar-nav justify-content-end pe-3">
             <li className="nav-item">
-              <Link to="/configuracion" className="nav-link" onClick={toggleMenu}>Configuracion</Link>
+              <Link to="/configuracion" className="nav-link" onClick={toggleMenu}>
+                <Settings className="nav-icon" /> Configuracion
+              </Link>
             </li>
             {store.isAuthenticated && (
               <li className="nav-item">
                 <button className="nav-link btn btn-link logout-button" onClick={handleLogout}>
-                  Logout
+                  <LogOut className="nav-icon" /> Logout
                 </button>
               </li>
             )}
